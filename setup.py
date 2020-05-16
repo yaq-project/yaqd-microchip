4e54e49727b03a2fca6ab7c004515020ac87e207
Blaise Thompson <blaise@untzag.com>
2020-05-16 01:17:57 +0000

    pin core version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md") as readme_file:
     readme = readme_file.read()
 
 
-requirements = ["yaqd-core"]
+requirements = ["yaqd-core>=2020.05.1", "smbus"]
 
 extra_requirements = {"dev": ["black", "pre-commit"]}
 extra_files = {"yaqd_microchip": ["VERSION"]}
